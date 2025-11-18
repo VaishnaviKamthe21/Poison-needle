4837492749a4ad9546d4cd5bd41ab49fc0a8865a
VaishnaviKamthe21 <vaishnavikamthe1884@gmail.com>
2025-11-18 23:00:39 +0530

    switched from intent based lr classification to using sentence transformer for nlp

--- chatbot.py
+++ chatbot.py
@@ -1,23 +1,29 @@
-import joblib
+from sentence_transformers import SentenceTransformer, util
+import pandas as pd
 from responses import responses
 
-model = joblib.load("chatbot_model.pkl")
-vectorizer = joblib.load("vectorizer.pkl")
+df = pd.read_csv("intents.csv")
 
-THRESHOLD = 0.5
+all_examples = df["question"].tolist()
+example_intents = df["intent"].tolist()
+
+model = SentenceTransformer('all-MiniLM-L6-v2')  
+example_embeddings = model.encode(all_examples)  
+
+def best_intent(user_input):
+    user_emb = model.encode([user_input])[0]
+    
+    sims = util.cos_sim(user_emb, example_embeddings)[0]
+    best_idx = sims.argmax().item()
+    best_score = sims[best_idx].item()
+    if best_score < 0.55:  
+        return None
+    return example_intents[best_idx]
 
 def get_response(user_input):
-    vec = vectorizer.transform([user_input])
-    
-    # probabilities for each class
-    probs = model.predict_proba(vec)[0]
-    intent = model.classes_[probs.argmax()]
-    confidence = probs.max()
-    
-    if confidence < THRESHOLD or intent not in responses:
-        # HTML link for Flask route
+    intent = best_intent(user_input)
+    if not intent or intent not in responses:
         return ("I don't have info on this. "
                 "Please submit a request/complaint "
                 "<a href='/complaint' target='_blank' style='color:#00f;'>here</a>.")
-    
-    return responses.get(intent)
+    return responses[intent]
